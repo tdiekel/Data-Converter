46cd42f30f666ce14f8226a045a4d35a920df98c
tdiekel <tdiekel@googlemail.com>
2019-05-20 11:04:40 +0200

    Fixed tf converter

--- converters/TFRecordConverter.py
+++ converters/TFRecordConverter.py
@@ -21,7 +21,8 @@ class TFRecordConverter(converters.BaseConverter):
         time.sleep(0.1)
         for image_set in self.image_sets:
             print('\nCreating tfrecord files for {} ...'.format(image_set))
-            self.gt_boxes = converters.generate_tfrecord(self.image_path, self.output_path, image_set, self.id2cat, self.gt_boxes)
+            self.gt_boxes = converters.generate_tfrecord(self.image_path, self.output_path, image_set,
+                                                         self.id2cat, self.gt_boxes)
 
         self._create_label_map_pbtxt()
 
@@ -35,18 +36,42 @@ class TFRecordConverter(converters.BaseConverter):
         self.csv_converter.images_copied = self.images_copied
         self.csv_converter.images_split = self.images_split
 
+        self.csv_converter.categories = self.categories
+        self.csv_converter.org_categories = self.org_categories
+
+        self.csv_converter.included_ids = self.included_ids
+        self.csv_converter.excluded_classes = self.excluded_classes
+
+        self.csv_converter.label_id_mapping = self.label_id_mapping
+        self.csv_converter.label_rearrange_mapping = self.label_rearrange_mapping
+
+        self.csv_converter.cat2id = self.cat2id
+        self.csv_converter.id2cat = self.id2cat
+        self.csv_converter.gt_boxes = self.gt_boxes
+
         self.csv_converter.images = self.images
         self.csv_converter.label = self.label
-
         self.csv_converter.image_sets = self.image_sets
 
     def _copy_values_from_csv_converter(self):
         self.images_copied = self.csv_converter.images_copied
         self.images_split = self.csv_converter.images_split
 
+        self.categories = self.csv_converter.categories
+        self.org_categories = self.csv_converter.org_categories
+
+        self.included_ids = self.csv_converter.included_ids
+        self.excluded_classes = self.csv_converter.excluded_classes
+
+        self.label_id_mapping = self.csv_converter.label_id_mapping
+        self.label_rearrange_mapping = self.csv_converter.label_rearrange_mapping
+
+        self.cat2id = self.csv_converter.cat2id
+        self.id2cat = self.csv_converter.id2cat
+        self.gt_boxes = self.csv_converter.gt_boxes
+
         self.images = self.csv_converter.images
         self.label = self.csv_converter.label
-
         self.image_sets = self.csv_converter.image_sets
 
     def _create_label_map_pbtxt(self):
